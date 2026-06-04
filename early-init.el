0129e2f57eb5fb6051588080fb94042ee63b53a4
sudhanv09 <mystogun125@gmail.com>
2026-06-04 17:12:14 +0800

    minor

--- early-init.el
+++ early-init.el
@@ -1,5 +1,8 @@
 ;;; early-init.el --- Early Emacs initialization -*- lexical-binding: t; -*-
 
+(defconst my-data-dir
+  (expand-file-name "emacs/" (or (getenv "XDG_DATA_HOME") "~/.local/share")))
+
 (setq package-enable-at-startup nil)
 
 (setq gc-cons-threshold (* 128 1024 1024))
@@ -9,7 +12,7 @@
 (setq inhibit-splash-screen t)
 
 (when (boundp 'native-comp-eln-load-path)
-  (startup-redirect-eln-cache (expand-file-name "emacs/eln-cache" "~/.local/share")))
+  (startup-redirect-eln-cache (expand-file-name "emacs/eln-cache" my-data-dir)))
 
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
--- init.el
+++ init.el
@@ -1,5 +1,16 @@
 ;;; init.el --- Emacs configuration -*- lexical-binding: t; -*-
 
+(setq auto-save-list-file-prefix (concat my-data-dir "auto-save-list/saves-"))
+(setq savehist-file (concat my-data-dir "history"))
+(setq project-list-file (concat my-data-dir "projects"))
+(setq tramp-persistency-file-name (concat my-data-dir "tramp"))
+(setq persp-save-dir (concat my-data-dir "persp-save"))
+
+;; Transient (Magit etc.)
+(setq transient-history-file  (concat my-data-dir "transient/history.el"))
+(setq transient-values-file   (concat my-data-dir "transient/values.el"))
+(setq transient-levels-file   (concat my-data-dir "transient/levels.el"))
+
 (add-to-list 'custom-theme-load-path (expand-file-name "themes" user-emacs-directory))
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
